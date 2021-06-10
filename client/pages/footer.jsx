469e3e782f95a490458aeaa551f33c961d8792a4
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-06-10 14:28:05 -0700

    Work for footer.

--- client/pages/footer.jsx
+++ client/pages/footer.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 class Footer extends React.Component {
   render() {
     return (
+      <div className="footer-container">
       <footer>
         <a href="#">
           <h4 className="footer-name">Budget-Pal</h4>
         </a>
       </footer>
+      </div>
     );
   }
 }
